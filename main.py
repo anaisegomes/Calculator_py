0368dbd835303bdb91272734fecac0fad4bc1db8
Anaíse Gomes <110070867+anaisegomes@users.noreply.github.com>
2025-11-20 23:23:34 -0300

    Enhance button styles in calculator UI
    Updated button styles and added font properties.

--- main.py
+++ main.py
@@ -24,19 +24,19 @@ frame_corpo.grid(row=1, column=0)
 
 # Criando botôes
 
-b_1 = Button(frame_corpo, text="C", width=11, height=2, background=cor4)
+b_1 = Button(frame_corpo, text="C", width=11, height=2, background=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_1.place(x=0, y=0)
 
-b_2 = Button(frame_corpo, text="%", width=5, height=2,background=cor4)
+b_2 = Button(frame_corpo, text="%", width=5, height=2,background=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_2.place(x=90, y=0)
 
-b_3 = Button(frame_corpo, text="/", width=5, height=2, background=cor5)
+b_3 = Button(frame_corpo, text="/", width=5, height=2, background=cor5, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_3.place(x=177, y=0)
 
-b_4 = Button(frame_corpo, text="7", width=5, height=2, background=cor5)
+b_4 = Button(frame_corpo, text="7", width=5, height=2, background=cor5, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_4.place(x=0, y=50)
 
-b_5 = Button(frame_corpo, text="8", width=5, height=2, background=cor5)
+b_5 = Button(frame_corpo, text="8", width=5, height=2, background=cor5, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_5.place(x=0, y=90)
 
 
